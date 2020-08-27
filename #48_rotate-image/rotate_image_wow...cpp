0d6aeec19d0dc47e9ee53244af4510076a957180
o-oppang <cmal0425@gmail.com>
2020-08-28 01:14:10 +0900

    Add rotation_image_wow... file

---
+++ #48_rotate-image/rotate_image_wow...cpp
@@ -0,0 +1,39 @@
+/**
+ * You are given an n x n 2D matrix representing an image.
+ * Rotate the image by 90 degrees (clockwise).
+ * 
+ * Note:
+ * 
+ * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+ * 
+ * Example 1:
+ * 
+ * Given input matrix = 
+ * [
+ *   [1,2,3],
+ *   [4,5,6],
+ *   [7,8,9]
+ * ],
+ * 
+ * rotate the input matrix in-place such that it becomes:
+ * [
+ *   [7,4,1],
+ *   [8,5,2],
+ *   [9,6,3]
+ * ]
+ */
+
+// 아래 위로 뒤집고 \ 방향으로 뒤집... wow
+class Solution {
+public:
+   void rotate(vector<vector<int> > &matrix) {
+    reverse(matrix.begin(), matrix.end());
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = i + 1; j < matrix[i].size(); ++j)
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+};
+
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Rotate Image.
+// Memory Usage: 7.2 MB, less than 59.96% of C++ online submissions for Rotate Image.
